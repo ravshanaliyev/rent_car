8c37d4f410adbce27e73ffea146a8f9fee7000c0
aliyev_mehrojbek <aliyevmehrojbek8006@gmail.com>
2024-09-25 18:29:03 +0500

    I added offer-cars component, But no completed responsive yet

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -61,7 +61,8 @@ const config: Config = {
 			borderRadius: {
 				lg: 'var(--radius)',
 				md: 'calc(var(--radius) - 2px)',
-				sm: 'calc(var(--radius) - 4px)'
+				sm: 'calc(var(--radius) - 4px)',
+				// cst: 'calc(var(--radius) - 10px)',
 			}
 		}
 	},
