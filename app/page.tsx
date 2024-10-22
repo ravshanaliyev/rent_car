3ca448ab9858c0eee09b759028e3e307496c5edb
Mirmansur <147687431+Mirmansur@users.noreply.github.com>
2024-10-22 11:48:30 +0500

    last

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import CarModels from "@/components/shared/car-models";
 import FilterCars from "@/components/shared/filter-cars";
+import FirstNavbar from "@/components/shared/firstnavbar";
 import Footer from "@/components/shared/footer";
 import Hero from "@/components/shared/hero";
 import Navbar from "@/components/shared/navbar";
@@ -9,6 +10,7 @@ import OfferCars from "@/components/shared/offer-cars";
 export default function Home() {
   return (
     <main>
+      <FirstNavbar />
       <Navbar />
       <Hero />
       <FilterCars />
